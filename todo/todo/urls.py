c1897f632ddf669eb28a0e23c9e46f91de93a270
shmd001 <cool.shoronov19@gmail.com>
2022-02-03 13:50:22 +0300

    Added default index view

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,8 +1,9 @@
 """todo URL Configuration"""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("base.urls"))
 ]
