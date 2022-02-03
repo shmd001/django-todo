bde27a0cd657bc55339dc8f0e97ffa85e99e79da
shmd001 <cool.shoronov19@gmail.com>
2022-02-03 22:05:20 +0300

    Added TaskDelete view

--- todo/base/views.py
+++ todo/base/views.py
@@ -2,7 +2,7 @@
 
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -37,4 +37,12 @@ class TaskUpdate(UpdateView):
     model = Task
     template_name = 'base/task_update.html'
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    """Task deleting view"""
+
+    model = Task
+    template_name = 'base/task_delete.html'
     success_url = reverse_lazy('tasks')
