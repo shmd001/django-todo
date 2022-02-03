c21a12ebe2b10939d116d1b556df6c4db9681a21
shmd001 <cool.shoronov19@gmail.com>
2022-02-03 13:34:24 +0300

    Deleteded integrated comments

--- todo/todo/asgi.py
+++ todo/todo/asgi.py
@@ -1,16 +1,9 @@
-"""
-ASGI config for todo project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
+"""ASGI config for todo project."""
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
 
 application = get_asgi_application()
--- todo/todo/wsgi.py
+++ todo/todo/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for todo project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
+"""WSGI config for todo project."""
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
 
 application = get_wsgi_application()
