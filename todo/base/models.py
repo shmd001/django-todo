70b0e036e451bb006874319970daacfa4febd71b
shmd001 <cool.shoronov19@gmail.com>
2022-02-03 14:15:50 +0300

    Created Task model

--- todo/base/models.py
+++ todo/base/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+"""Base app models"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    """Task Model"""
+
+    user = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """Meta class for ordering tasks"""
+
+        ordering = ['-created']
