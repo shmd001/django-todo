6da93d43aa77ea890db644c071e185c3b2e51892
shmd001 <cool.shoronov19@gmail.com>
2022-02-03 14:43:24 +0300

    Created TaskDetail view

--- todo/base/views.py
+++ todo/base/views.py
@@ -1,9 +1,20 @@
 """Base app class based views"""
 
-from .models import Task
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
 
 class TaskList(ListView):
+    """Task list view"""
+
     model = Task
     context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    """Task detail view"""
+
+    model = Task
+    template_name = 'base/task.html'
+    context_object_name = 'task'
